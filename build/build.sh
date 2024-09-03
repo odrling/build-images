4d3b8d51965c7a1eb818e8634a7396b371958c6d
odrling <florianbadie@odrling.xyz>
2024-09-03 10:49:38 +0200

    build zlib directly

--- build/build.sh
+++ build/build.sh
@@ -1,27 +1,44 @@
-#!/bin/sh -ex
+#!/bin/sh
+set -e
+GCC_VER=14
 if [ -z "${TARGET}" ]; then
     cross_args="--native-file /build/native.ini"
-    apt-get -y install gcc-14 g++-14
+    apt-get -y install gcc-${GCC_VER} g++-${GCC_VER}
 else
+    arch=$(echo ${TARGET} | cut -d- -f1)
+    cmake_args="-DCMAKE_TOOLCHAIN_FILE=/build/${TARGET}.cmake -DCMAKE_INSTALL_PREFIX=/usr/${TARGET}"
+    ffmpeg_args="--cross-prefix=${TARGET}- --pkg-config=pkg-config --cc=${TARGET}-gcc-${GCC_VER} --prefix=/usr/${TARGET} --arch=${arch} --target-os=linux"
     cross_args="--cross-file /build/${TARGET}.ini"
-    apt-get -y install gcc-14-aarch64-linux-gnu g++-14-aarch64-linux-gnu qemu-user-static
+    apt-get -y install gcc-${GCC_VER}-${TARGET} g++-${GCC_VER}-${TARGET} qemu-user-static
+    export PKG_CONFIG_SYSROOT_DIR="/usr/${TARGET}"
 fi
 
-if [ ! -d /deps/ffmpeg ]; then
-    git clone --depth 1 --branch meson-7.0 https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg.git /deps/ffmpeg
+if [ ! -d /deps/zlib ]; then
+    git clone --depth 1 -b master https://github.com/madler/zlib.git /deps/zlib
 fi
-
-meson setup /deps/ffmpeg_build /deps/ffmpeg --reconfigure --buildtype release -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Db_sanitize=undefined --auto-features=disabled -Ddefault_library=shared -Dtests=disabled -Dprograms=disabled -Dencoders=disabled -Dmuxers=disabled -Davfilter=disabled -Davdevice=disabled -Dpostproc=disabled -Dswresample=disabled -Dswscale=disabled -Daac_decoder=enabled -Dversion3=enabled $cross_args
-meson install -C /deps/ffmpeg_build
+mkdir -p /deps/zlib_build
+cd /deps/zlib_build
+cmake ${cmake_args} /deps/zlib
+make -j$(ncproc)
+make install
 
 if [ ! -d /deps/libass ]; then
     git clone --depth 1 https://github.com/libass/libass.git /deps/libass
+    ln -s /build/subprojects /deps/libass
 fi
 
-cd /deps/libass
-cp -r /build/subprojects .
-meson setup /deps/libass_build /deps/libass --reconfigure --buildtype release -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Db_sanitize=undefined --auto-features=disabled -Ddefault_library=shared -Dasm=enabled -Dfontconfig=enabled -Dfribidi:bin=false $cross_args
+meson setup /deps/libass_build /deps/libass --reconfigure --buildtype release -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Db_sanitize=undefined --auto-features=disabled -Ddefault_library=shared -Dasm=enabled -Dfontconfig=enabled -Dzlib:default_library=shared -Dfribidi:bin=false $cross_args
 meson install -C /deps/libass_build
+
+if [ ! -d /deps/ffmpeg ]; then
+    git clone --depth 1 --branch release/7.0 https://github.com/FFmpeg/FFmpeg.git /deps/ffmpeg
+fi
+
+mkdir -p /deps/ffmpeg_build
+cd /deps/ffmpeg_build
+/deps/ffmpeg/configure --logfile=/dev/stderr --enable-shared --disable-static --disable-autodetect --disable-programs --disable-avdevice --disable-postproc --disable-avfilter --disable-swscale --disable-swresample --disable-doc --disable-muxers --disable-network --disable-encoders --disable-decoders --disable-bsfs --disable-protocols --enable-zlib --enable-decoder=aac --toolchain=hardened --enable-lto=auto ${ffmpeg_args}
+make -j$(nproc)
+make install
 
 rm -rf /deps /build
 apt-get clean
