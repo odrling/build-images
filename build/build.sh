aeeaa18773cd5c8614b080b738350f5da96f21e0
odrling <florianbadie@odrling.xyz>
2026-03-07 23:29:04 +0100

    just create gcc symlinks

--- build/build.sh
+++ build/build.sh
@@ -6,8 +6,8 @@ GCL_VER=v2.11.1
 if [ -z "${TARGET}" ]; then
     cross_args="--native-file /build/native.ini"
     apt-get -y install gcc-${GCC_VER} g++-${GCC_VER}
-    export CC=gcc-${GCC_VER}
-    export CXX=g++-${GCC_VER}
+    ln -s /usr/bin/gcc-${GCC_VER} /usr/bin/gcc
+    ln -s /usr/bin/g++-${GCC_VER} /usr/bin/gcc
 else
     arch=$(echo ${TARGET} | cut -d- -f1)
     cmake_args="-DCMAKE_TOOLCHAIN_FILE=/build/${TARGET}.cmake -DCMAKE_INSTALL_PREFIX=/usr/${TARGET}"
